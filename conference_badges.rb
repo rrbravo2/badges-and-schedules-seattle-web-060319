dc2fc5862800d2a3cf9a071eea7016f9f710c620
Ryan Bravo <rrbravo2@gmail.com>
2019-05-22 04:50:24 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,3 +1,8 @@
 def badge_maker(name)
  "Hello, my name is #{name}."
 end # Write your code here.
+
+def batch_badge_creator(attendees)
+ attendees.map do |attendee|
+  "Hello, my name is #{attendee}" 
+end
